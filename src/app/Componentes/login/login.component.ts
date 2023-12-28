268df9739972a27cc0ef4ce08a6bd7013bee2e9c
Erick Mata Torres <abcdefghij110@outlook.com>
2023-12-28 12:29:04 -0700

    Haciendo Login

---
+++ src/app/Componentes/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatInputModule, ReactiveFormsModule, FormsModule, MatFormFieldModule, MatButtonModule, MatIconModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+}
